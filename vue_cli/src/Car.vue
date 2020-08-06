3e984948be41861a1f73cb0d361ae0957e2db7ff
Zotoff <paulzotoff@gmail.com>
2020-08-06 15:28:31 +0300

    Started CLI

---
+++ vue_cli/src/Car.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="card">
+        <h3>Name: {{ carName }}</h3>
+        <p>Year: {{ carYear }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            carName: 'Ford',
+            carYear: 2020
+        }
+    }
+}
+</script>
