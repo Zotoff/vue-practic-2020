a76a1f99de60835f137e9c9113a55cd015b5692d
Zotoff <paulzotoff@gmail.com>
2020-08-12 12:52:08 +0300

    Started routing and validation sections

---
+++ vue_validation/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="card">
+                <div class="card-body">
+                  <form>
+                      <div class="form-group">
+                        <label for="email">Email</label>
+                        <input
+                        class="form-control"
+                        :class="{'is-invalid': $v.email.$error}"
+                        type="email"
+                        v-model="email" @blur="$v.email.$touch"/>
+                        <div class="invalid-feedback">Invalid feedback</div>
+                      </div>
+                  </form>
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { required } from 'vuelidate/lib/validators'
+export default {
+  data () {
+    return {
+      email: ''
+    }
+  },
+  validations: {
+    email: {
+      required: required
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue_validation/src/Form.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="card">
+        <div class="card-body">
+            
+        </div>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
